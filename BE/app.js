a4f9b8e7ed8be7b90ba6f6ec871581001504408e
jmgb27 <mark.john6672@gmail.com>
2023-02-28 22:18:47 +0800

    BE port change

--- BE/app.js
+++ BE/app.js
@@ -129,6 +129,6 @@ menu_stream.on("change", async () => {
 });
 
 
-server.listen(3000, () => {
-    console.log("Server started on port 3000");
+server.listen(8080, () => {
+    console.log("Server started on port 8080");
 });
