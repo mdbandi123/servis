bb44d007b13875ac9aa23983063c1b4af2b24a74
jmgb27 <mark.john6672@gmail.com>
2023-03-12 14:41:21 +0800

    remove Docker, filtered items, Nav item icon notif

--- FE/client-app-customer/src/Navigation.js
+++ FE/client-app-customer/src/Navigation.js
@@ -1,23 +1,27 @@
-import * as React from 'react';
-import { useState } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import * as React from "react";
+import { useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
 
-import { useScrollTrigger, Stack, Box, Paper, Avatar } from '@mui/material/';
-import { BottomNavigation, BottomNavigationAction, AppBar, Toolbar } from '@mui/material/';
-import { teal, orange, grey } from '@mui/material/colors';
-import AssignmentIcon from '@mui/icons-material/Assignment';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import PaymentIcon from '@mui/icons-material/Payment';
-import PendingActionsIcon from '@mui/icons-material/PendingActions';
+import { useScrollTrigger, Stack, Box, Paper, Avatar } from "@mui/material/";
+import {
+    BottomNavigation,
+    BottomNavigationAction,
+    AppBar,
+    Toolbar,
+} from "@mui/material/";
+import { teal, orange, grey } from "@mui/material/colors";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PaymentIcon from "@mui/icons-material/Payment";
+import PendingActionsIcon from "@mui/icons-material/PendingActions";
 
-import GlobalWhiteHeader6 from './global/typographies/headers/WhiteHeader6';
-import GlobalTealBadge from './global/badges/TealBadge';
+import GlobalWhiteHeader6 from "./global/typographies/headers/WhiteHeader6";
+import GlobalTealBadge from "./global/badges/TealBadge";
 
-import store from './store/store';
+import store from "./store/store";
 
 function ElevationScroll(props) {
-
     const { children, window } = props;
 
     const trigger = useScrollTrigger({
@@ -37,59 +41,156 @@ ElevationScroll.propTypes = {
 };
 
 export default function Navigation(props) {
-    const [value, setValue] = useState('menu');
+    const [value, setValue] = useState("menu");
     const navigate = useNavigate();
 
+    const { setCartItems, orderedItems, setOrderedItems } = store();
+
     const table_number = store((state) => state.table_number);
+    const order_id = store((state) => state.order_id);
+    const cartItems = store((state) => state.cartItems);
+
+    const pendingItems = orderedItems.filter(
+        (item) => item.status !== "served"
+    );
+
+    React.useEffect(() => {
+        fetch(
+            `${process.env.REACT_APP_BACKEND_URL}/order_items/cart/${
+                order_id || localStorage.getItem("order_id")
+            }`,
+            {
+                method: "GET",
+            }
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                setCartItems(data.items);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
+    React.useEffect(() => {
+        fetch(
+            `${process.env.REACT_APP_BACKEND_URL}/order_items/items/${
+                order_id || localStorage.getItem("order_id")
+            }`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.success) {
+                    setOrderedItems(data.items);
+                } else {
+                    console.log(data.error);
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
     const navigation = {
-        position: 'fixed',
+        position: "fixed",
         bottom: 0,
         left: 0,
         right: 0,
-        zIndex: 1
+        zIndex: 1,
     };
 
     const avatar = {
-        bgcolor: grey[500]
+        bgcolor: grey[500],
     };
 
     const navigationSelector = {
-        '&.Mui-selected': {
-            color: orange[700]
-        }
+        "&.Mui-selected": {
+            color: orange[700],
+        },
     };
 
     const appBar = {
-        backgroundColor: orange[700]
-    }
+        backgroundColor: orange[700],
+    };
 
     return (
         <React.Fragment>
             <ElevationScroll {...props}>
                 <AppBar sx={appBar}>
                     <Toolbar>
-                        <Stack direction='row' alignItems='center' spacing={1}>
+                        <Stack direction="row" alignItems="center" spacing={1}>
                             <Box>
                                 <Avatar sx={avatar} />
                             </Box>
                             <Box>
                                 <GlobalWhiteHeader6 text={`${table_number}`} />
-                            </Box >
-                        </Stack >
-                    </Toolbar >
-                </AppBar >
-            </ElevationScroll >
+                            </Box>
+                        </Stack>
+                    </Toolbar>
+                </AppBar>
+            </ElevationScroll>
             <Toolbar />
-            <Outlet/>
+            <Outlet />
             <Paper sx={navigation} elevation={10}>
-                <BottomNavigation showLabels value={value} onChange={(event, value) => setValue(value)} >
-                    <BottomNavigationAction sx={ navigationSelector } onClick={() => navigate('/')} label='Menu' value='menu' icon={<AssignmentIcon />} />
-                    <BottomNavigationAction sx={ navigationSelector } onClick={() => navigate('/cart')} label='Cart' value='cart' icon={<GlobalTealBadge badgeContent='10' max='9' overlap='circular'><ShoppingCartIcon /> </GlobalTealBadge>} />
-                    <BottomNavigationAction sx={ navigationSelector } onClick={() => navigate('/pending')} label='Pending' value='pending' icon={<GlobalTealBadge badgeContent='10' max='9' overlap='circular'><PendingActionsIcon /></GlobalTealBadge>} />
-                    <BottomNavigationAction sx={ navigationSelector } onClick={() => navigate('/payment')} label='Payment' value='payment' icon={<PaymentIcon />} />
+                <BottomNavigation
+                    showLabels
+                    value={value}
+                    onChange={(event, value) => setValue(value)}
+                >
+                    <BottomNavigationAction
+                        sx={navigationSelector}
+                        onClick={() => navigate("/")}
+                        label="Menu"
+                        value="menu"
+                        icon={<AssignmentIcon />}
+                    />
+                    <BottomNavigationAction
+                        sx={navigationSelector}
+                        onClick={() => navigate("/cart")}
+                        label="Cart"
+                        value="cart"
+                        icon={
+                            <GlobalTealBadge
+                                badgeContent={cartItems ? cartItems.length : 0}
+                                max="9"
+                                overlap="circular"
+                            >
+                                <ShoppingCartIcon />{" "}
+                            </GlobalTealBadge>
+                        }
+                    />
+                    <BottomNavigationAction
+                        sx={navigationSelector}
+                        onClick={() => navigate("/pending")}
+                        label="Pending"
+                        value="pending"
+                        icon={
+                            <GlobalTealBadge
+                                badgeContent={
+                                    pendingItems ? pendingItems.length : 0
+                                }
+                                max="9"
+                                overlap="circular"
+                            >
+                                <PendingActionsIcon />
+                            </GlobalTealBadge>
+                        }
+                    />
+                    <BottomNavigationAction
+                        sx={navigationSelector}
+                        onClick={() => navigate("/payment")}
+                        label="Payment"
+                        value="payment"
+                        icon={<PaymentIcon />}
+                    />
                 </BottomNavigation>
             </Paper>
-        </React.Fragment >
+        </React.Fragment>
     );
 }
--- FE/client-app-customer/src/pages/menu/CategoryList.js
+++ FE/client-app-customer/src/pages/menu/CategoryList.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import useStore from '../../store/store';
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import useStore from "../../store/store";
 
-import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
-import { Box, Stack, Card, CardContent, CardMedia, IconButton } from '@mui/material';
-import { grey } from '@mui/material/colors';
-import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
-import AddRoundedIcon from '@mui/icons-material/AddRounded';
-import ExtensionOffIcon from '@mui/icons-material/ExtensionOff';
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import {
+    Box,
+    Stack,
+    Card,
+    CardContent,
+    CardMedia,
+    IconButton,
+} from "@mui/material";
+import { grey } from "@mui/material/colors";
+import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
+import AddRoundedIcon from "@mui/icons-material/AddRounded";
+import ExtensionOffIcon from "@mui/icons-material/ExtensionOff";
 
-import GlobalBlackHeader6 from '../../global/typographies/headers/BlackHeader6';
-import GlobalBlackHeader4 from '../../global/typographies/headers/BlackHeader4';
-import GlobalOrangeHeader6 from '../../global/typographies/headers/OrangeHeader6';
-import GlobalGreyBody2 from '../../global/typographies/bodies/GreyBody2';
-import GlobalTealContainedButton from '../../global/buttons/contains/TealContainedButton';
-import GlobalBlackHeader5 from '../../global/typographies/headers/BlackHeader5';
-import FadeIn from '../../global/animation/FadeIn';
+import GlobalBlackHeader6 from "../../global/typographies/headers/BlackHeader6";
+import GlobalBlackHeader4 from "../../global/typographies/headers/BlackHeader4";
+import GlobalOrangeHeader6 from "../../global/typographies/headers/OrangeHeader6";
+import GlobalGreyBody2 from "../../global/typographies/bodies/GreyBody2";
+import GlobalTealContainedButton from "../../global/buttons/contains/TealContainedButton";
+import GlobalBlackHeader5 from "../../global/typographies/headers/BlackHeader5";
+import FadeIn from "../../global/animation/FadeIn";
 
 function CategoryList() {
     const navigate = useNavigate();
@@ -25,7 +32,9 @@ function CategoryList() {
     const { order_id } = useStore();
 
     React.useEffect(() => {
-        fetch(`${process.env.REACT_APP_BACKEND_URL}/menu_items/items/${category_name}`)
+        fetch(
+            `${process.env.REACT_APP_BACKEND_URL}/menu_items/items/${category_name}`
+        )
             .then((response) => response.json())
             .then((data) => {
                 if (data.success) {
@@ -34,35 +43,37 @@ function CategoryList() {
                 } else {
                     console.log(data.error);
                 }
-            }
-            ).catch((error) => {
+            })
+            .catch((error) => {
                 console.log(error);
             });
-    }, [])
+    }, []);
 
     const addToCart = (item_id) => {
         console.log(item_id);
         fetch(`${process.env.REACT_APP_BACKEND_URL}/order_items/item/`, {
-            method: 'POST',
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
             },
             body: JSON.stringify({
                 item_id: item_id,
                 quantity: 1,
-                order_id: order_id
-            })
-        }).then((response) => response.json())
+                order_id: order_id,
+            }),
+        })
+            .then((response) => response.json())
             .then((data) => {
                 if (data.success) {
                     console.log(data.message);
                 } else {
                     console.log(data.error);
                 }
-            }).catch((error) => {
+            })
+            .catch((error) => {
                 console.log(error);
             });
-    }
+    };
 
     const pageContainer = {
         pb: 8,
@@ -73,7 +84,13 @@ function CategoryList() {
 
     const headerPage = {
         pb: 2,
-        textAlign: { xs: 'left', sm: 'left', md: 'center', lg: 'center', lx: 'center' }
+        textAlign: {
+            xs: "left",
+            sm: "left",
+            md: "center",
+            lg: "center",
+            lx: "center",
+        },
     };
 
     const cardContainer = {
@@ -81,40 +98,46 @@ function CategoryList() {
     };
 
     const cardContent = {
-        textAlign: 'center'
+        textAlign: "center",
     };
 
     const itemNamePrice = {
-        fontSize: '1.1em'
+        fontSize: "1.1em",
     };
 
     const centerAlignment = {
         pt: 3,
-        textAlign: 'center',
-        justifyContent: 'center',
-        alignItems: 'center',
-        display: 'flex'
+        textAlign: "center",
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
     };
 
     const noItemIcon = {
-        fontSize: '6em',
-        color: grey[600]
+        fontSize: "6em",
+        color: grey[600],
     };
 
     const addBtn = {
-        width: '100%'
+        width: "100%",
     };
 
     const categoryNamePriceContainer = {
-        height: 90
+        height: 90,
     };
 
     if (StartersData.length === 0) {
         return (
             <FadeIn>
                 <Box sx={pageContainer}>
-                    <Stack sx={headerPage} direction='row' alignItems='center' alignContent='center' spacing={1}>
-                        <IconButton onClick={() => navigate('/')}>
+                    <Stack
+                        sx={headerPage}
+                        direction="row"
+                        alignItems="center"
+                        alignContent="center"
+                        spacing={1}
+                    >
+                        <IconButton onClick={() => navigate("/")}>
                             <ArrowBackRoundedIcon />
                         </IconButton>
                         <GlobalBlackHeader4 text={category_name} />
@@ -124,41 +147,86 @@ function CategoryList() {
                             <ExtensionOffIcon sx={noItemIcon} />
                         </Grid2>
                         <Grid2 item xs={12} sm={12} md={12} lg={12} lx={12}>
-                            <GlobalBlackHeader5 text={`No ${category_name} Found`} />
+                            <GlobalBlackHeader5
+                                text={`No ${category_name} Found`}
+                            />
                         </Grid2>
                         <Grid2 item xs={12} sm={12} md={12} lg={12} lx={12}>
-                            <GlobalGreyBody2 text={`We couldn't find any ${category_name}. Admin might not have created yet.`} />
+                            <GlobalGreyBody2
+                                text={`We couldn't find any ${category_name}. Admin might not have created yet.`}
+                            />
                         </Grid2>
                     </Grid2>
                 </Box>
             </FadeIn>
         );
-    };
+    }
 
     return (
         <FadeIn>
             <Box sx={pageContainer}>
-                <Stack sx={headerPage} direction='row' alignItems='center' alignContent='center' spacing={1}>
-                    <IconButton onClick={() => navigate('/')}>
+                <Stack
+                    sx={headerPage}
+                    direction="row"
+                    alignItems="center"
+                    alignContent="center"
+                    spacing={1}
+                >
+                    <IconButton onClick={() => navigate("/")}>
                         <ArrowBackRoundedIcon />
                     </IconButton>
                     <GlobalBlackHeader4 text={category_name} />
                 </Stack>
-                <Grid2 container justifyContent='start'>
+                <Grid2 container justifyContent="start">
                     <Grid2 item>
-                        <Grid2 container justifyContent='start' spacing={1}>
-                            {StartersData.map((startersList) => (
+                        <Grid2 container justifyContent="start" spacing={1}>
+                            {StartersData.filter(
+                                (startersList) => startersList.is_available
+                            ).map((startersList) => (
                                 <Grid2 item xs={6} sm={6} md={4} lg={3} lx={3}>
                                     <Card sx={cardContainer}>
-                                        <CardMedia component='img' height='140' image={`${process.env.REACT_APP_BACKEND_URL}${startersList.image}`} alt={startersList.name} />
+                                        <CardMedia
+                                            component="img"
+                                            height="140"
+                                            image={`${process.env.REACT_APP_BACKEND_URL}${startersList.image}`}
+                                            alt={startersList.name}
+                                        />
                                         <CardContent sx={cardContent}>
-                                            <Stack direction='column' spacing={2}>
-                                                <Box sx={categoryNamePriceContainer}>
-                                                    <GlobalBlackHeader6 sx={itemNamePrice} text={startersList.name} />
-                                                    <GlobalOrangeHeader6 sx={itemNamePrice} text={'₱' + startersList.price.$numberDecimal} />
+                                            <Stack
+                                                direction="column"
+                                                spacing={2}
+                                            >
+                                                <Box
+                                                    sx={
+                                                        categoryNamePriceContainer
+                                                    }
+                                                >
+                                                    <GlobalBlackHeader6
+                                                        sx={itemNamePrice}
+                                                        text={startersList.name}
+                                                    />
+                                                    <GlobalOrangeHeader6
+                                                        sx={itemNamePrice}
+                                                        text={
+                                                            "₱" +
+                                                            startersList.price
+                                                                .$numberDecimal
+                                                        }
+                                                    />
                                                 </Box>
-                                                <Box >
-                                                    <GlobalTealContainedButton text='Add to cart' sx={addBtn} startIcon={<AddRoundedIcon />} onClick={() => addToCart(startersList._id)} />
+                                                <Box>
+                                                    <GlobalTealContainedButton
+                                                        text="Add to cart"
+                                                        sx={addBtn}
+                                                        startIcon={
+                                                            <AddRoundedIcon />
+                                                        }
+                                                        onClick={() =>
+                                                            addToCart(
+                                                                startersList._id
+                                                            )
+                                                        }
+                                                    />
                                                 </Box>
                                             </Stack>
                                         </CardContent>
